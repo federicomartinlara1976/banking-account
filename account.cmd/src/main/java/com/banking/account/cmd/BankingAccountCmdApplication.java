ff775e3016716b2a6b414b5f93313e33a1324b08
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2024-06-13 21:46:30 +0200

    Register handlers

--- account.cmd/src/main/java/com/banking/account/cmd/BankingAccountCmdApplication.java
+++ account.cmd/src/main/java/com/banking/account/cmd/BankingAccountCmdApplication.java
@@ -1,15 +1,39 @@
 package com.banking.account.cmd;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import com.banking.account.cmd.api.command.CloseAccountCommand;
+import com.banking.account.cmd.api.command.CommandHandler;
+import com.banking.account.cmd.api.command.DepositFundsCommand;
+import com.banking.account.cmd.api.command.OpenAccountCommand;
+import com.banking.account.cmd.api.command.WithdrawFundsCommand;
+import com.banking.cqrs.core.commands.CommandHandlerMethod;
+import com.banking.cqrs.core.infrastructure.CommandDispatcher;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BankingAccountCmdApplication {
+	
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+	
+	@Autowired
+	private CommandHandler commandHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAccountCmdApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
 }
