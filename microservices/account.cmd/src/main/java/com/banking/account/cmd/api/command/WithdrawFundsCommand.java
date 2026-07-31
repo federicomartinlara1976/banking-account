3da870bb015e19631dc8715b580423b100526a42
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-07-31 20:06:01 +0200

    Se corrige generación JSON

--- microservices/account.cmd/src/main/java/com/banking/account/cmd/api/command/WithdrawFundsCommand.java
+++ microservices/account.cmd/src/main/java/com/banking/account/cmd/api/command/WithdrawFundsCommand.java
@@ -1,17 +1,22 @@
 package com.banking.account.cmd.api.command;
 
 import com.banking.cqrs.core.commands.BaseCommand;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 public class WithdrawFundsCommand extends BaseCommand {
 
-	public WithdrawFundsCommand(String id) {
+	@JsonCreator
+	public WithdrawFundsCommand(@JsonProperty("id") String id) {
 		super(id);
 	}
 
 	@Getter
 	@Setter
+	@Positive(message = "La retirada debe ser mayor que 0")
 	private Double amount;
 }
